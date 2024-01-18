74e733a86bcf0ba0fe95a2c397ef91e1531a9e45
Shivang Srivastava <53571420+shivang-git@users.noreply.github.com>
2024-01-18 23:38:13 +0530

    redux setup

--- qunnect-frontend/src/app/store.js
+++ qunnect-frontend/src/app/store.js
@@ -1,1 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from '../features/user/userSlice'
+import postReducer from '../features/products/productSlice'
+export const store = configureStore({
+  reducer: {
+    users:userReducer,
+    posts:postReducer
+  },
+});
